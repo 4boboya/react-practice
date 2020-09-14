b8f045cd2d8774d08f0b819caaf4ee72c6e2a7fc
dear91304526 <dear91304526@gmail.com>
2020-09-14 16:11:55 +0800

    der

--- src/Cheer.js
+++ src/Cheer.js
@@ -7,7 +7,7 @@ const Cheer=(props)=>{
 
     return(
         <div>
-            <h1>波波超棒</h1>
+            <h1>波波超棒的唷</h1>
             <div>目前分數：{score}分 還差：{88-props.value}分</div>
             <button value={Number(props.value)+1} onClick={props.onClick}>加一分</button>
             <button value={Number(props.value)+7} onClick={props.onClick}>加七分</button>
